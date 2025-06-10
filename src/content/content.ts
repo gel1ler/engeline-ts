76d5b2db1a4118a4d796ebf3fb89f1c9da191c69
gel1ler <57824633+gel1ler@users.noreply.github.com>
2025-06-10 16:38:55 +0300

    Update phone

--- src/content/content.ts
+++ src/content/content.ts
@@ -3,7 +3,7 @@ import { IMainInfo, INavLink, ISocialMedia } from "./types";
 export const mainInfo: IMainInfo = {
     name: 'Название',
     description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione officia a iure dolore impedit velit, blanditiis repellat ipsa alias nobis illo iusto, cupiditate perferendis natus omnis iste, libero asperiores! Quis.',
-    phone: '+7(495)374-53-43',
+    phone: '+7(909)993-43-33',
     email: 'info@engeline.ru',
     map: 'https://yandex.ru/maps/?from=mapframe&ll=43.524819%2C54.993879&mode=usermaps&source=mapframe&um=constructor%3A711ea37dc3a1461d53cb66acc3637e91b23c8492ed19997ddb60495aa5a2ae09&utm_source=mapframe&z=5',
     logoSmall: '/logo/logo-small.svg',
